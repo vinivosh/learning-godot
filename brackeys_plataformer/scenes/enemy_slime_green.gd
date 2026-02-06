fcff19dd26162f81512e6640e3b1bf4ff89e836f
vinivosh <36175157+vinivosh@users.noreply.github.com>
2026-02-06 18:35:09 -0300

    💫 Impl. automatic side-to-side mov. to Slime

---
+++ brackeys_plataformer/scenes/enemy_slime_green.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+@onready var ray_cast_right: RayCast2D = $RayCastRight
+@onready var ray_cast_left: RayCast2D = $RayCastLeft
+
+const SPEED = 60
+var direction = 1
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if ray_cast_right.is_colliding():
+		direction = -1
+	if ray_cast_left.is_colliding():
+		direction = 1
+
+	position.x += direction * SPEED * delta
