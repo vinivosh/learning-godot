2d9e0265fc175b7494dfc20f4fbf16767ae4c121
vinivosh <36175157+vinivosh@users.noreply.github.com>
2026-03-10 15:48:51 -0300

    ✨ Add coin pickup sound effect

--- brackeys_plataformer/scripts/coin.gd
+++ brackeys_plataformer/scripts/coin.gd
@@ -1,9 +1,10 @@
 extends Area2D
 
 
-@onready var game_manager: Node = %GameManager
+@onready var game_mngr: Node = %GameManager
+@onready var anim_player: AnimationPlayer = $AnimationPlayer
 
 
 func _on_body_entered(_body: Node2D) -> void:
-	game_manager.add_points(1)
-	queue_free()
+	game_mngr.add_points(1)
+	anim_player.play("CoinPickUp")
